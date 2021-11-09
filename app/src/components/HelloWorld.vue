12b5191a42d9d7620c0bef519b980f36a4a25fac
lucyk22 <lucy.kwan22@sitechhs.com>
2021-11-09 12:29:05 -0500

    crying in da club

--- app/src/components/HelloWorld.vue
+++ app/src/components/HelloWorld.vue
@@ -1,13 +1,44 @@
 <template>
-  <div>honeyhunter ripoff</div>
+  <div class="character-card">
+      <span>{{characters}} wah</span>
+      <img src="https://api.genshin.dev/characters/xiao/icon-big" alt="character icon">
+      <h3 class="character-name">Xiao</h3>
+  </div>
+
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      characters: ["xiao"],
+    }
+  },
+created() {
+    this.birth
+  },
+  methods: {
+    birth: function() {
+      console.log('humu');
+      this.characters = ['albedo'];
+
+    }
+  }
 
 }
 </script>
 
 <style>
-
+.character-card {
+  background-color: rgb(26, 26, 29) ;
+  display: flex;
+  flex-direction: column;
+  color: white;
+  width: 10rem;
+  height: 20rem;
+  padding: 1rem;
+  align-items: center;
+  margin: 2rem;
+  border-radius: 5%;
+}
 </style>
--- app/src/views/Home.vue
+++ app/src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    a
+    home
   </div>
 </template>
 
