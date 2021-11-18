0a435c7231ba3a2f3c1959bb6e9673fd49115db0
lucyk22 <lucy.kwan22@sitechhs.com>
2021-11-18 12:23:24 -0500

    back to zero (pain)

--- app/src/components/card.vue
+++ app/src/components/card.vue
@@ -21,9 +21,43 @@ export default {
       charaList: [""],
       character: [""],
       icon: [],
- 
     }
   },
+  created() {
+    this.fetchData()
+  },
+  methods: {
+    fetchData: async function() {
+
+      //icon mcchangerino - charalist is megalist of all charas, then foreach?
+        try {
+          const response = await fetch('https://api.genshin.dev/characters');
+          const data = await response.json();
+          this.charaList = data;
+          
+          this.charaList.forEach(element => {
+              this.icon.push(`https://api.genshin.dev/characters/${element}/icon`) ;
+            
+          });
+          
+      } catch (error) {
+          console.log('error');
+      }
+      
+      //character data info thingy idk my brain is falling apart?
+      try {
+        const response = await fetch(`https://api.genshin.dev/characters/${this.charaList[0]}`);
+          const data = await response.json();
+          this.character.push(data);
+          
+
+      } catch (error) {
+          console.log(error);
+      }
+
+
+    },
+  }
 }
 </script>
 
